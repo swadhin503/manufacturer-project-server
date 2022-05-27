1e158bead8af77294eeb4051e428c1bfc9541c1d
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-05-28 00:15:35 +0600

    admin checking added

--- index.js
+++ index.js
@@ -67,7 +67,36 @@ async function run(){
             var token = jwt.sign({ email:email }, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'});
             res.send({result,token});
         })
-        
+        // for admin
+        app.put('/user/admin/:email', async (req, res)=>{
+            const email = req.params.email;
+            const requester = req.params.email;
+            const requesterAccount = await usersCollection.findOne({email:requester});
+            if(!requesterAccount.role === 'admin'){
+                const filter = {email: email};
+            const updateDoc = {
+                $set:{role:'admin'},
+            };
+            const result = await usersCollection.updateOne(filter,updateDoc);
+            res.send({result});
+            }
+            else{
+                res.status(403).send({message: 'forbidden'});
+            }
+        })
+        // for getting all user
+        app.get('/user', async (req, res) => {
+            
+            const users = await usersCollection.find().toArray();
+            res.send(users);
+        })
+        // checking admin or not
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin});
+        })
         // for inserting one item in orders collection
         app.post('/order', async (req, res)=>{
             const newItem = req.body;
